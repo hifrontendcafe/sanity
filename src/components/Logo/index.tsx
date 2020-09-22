ce0322f416ff4eea6e37b933867ea778149d7c26
Martin Bavio <mbavio@gmail.com>
2020-09-22 02:47:39 -0300

    Add basic styling

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.css';
+
+const Logo = () => (
+  <div className={styles.logo}>
+    <img src="/static/fec-logo.svg" alt="Frontend Cafe" />
+  </div>
+);
+
+export default Logo;
