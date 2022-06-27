8702eb5f76612cee4ca481b8922c743845e9e8de
Francisco Peralta <fran.mper@gmail.com>
2022-06-27 10:26:22 -0300

    add cmyk 5

--- src/schemas/cmyk-participant.ts
+++ src/schemas/cmyk-participant.ts
@@ -14,6 +14,25 @@ export default {
       validation: (Rule) => Rule.required(),
     },
     {
+      title: 'Tipo de participación',
+      name: 'participationType',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Lider', value: 'lider' },
+          { title: 'Participante', value: 'participant' },
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'isChix',
+      title:
+        'Tiene el rol Chix',
+      type: 'boolean',
+      validation: (Rule) => Rule.required(),
+    },
+    {
       title: 'CMYK Version',
       name: 'cmykVersion',
       type: 'string',
@@ -23,6 +42,7 @@ export default {
           { title: 'v2.0', value: '2' },
           { title: 'v3.0', value: '3' },
           { title: 'v4.0', value: '4' },
+          { title: 'v5.0', value: '5' },
         ],
       },
       validation: (Rule) => Rule.required(),
@@ -43,6 +63,47 @@ export default {
       validation: (Rule) => Rule.required(),
     },
     {
+      name: 'experience',
+      title: '¿Ya tienes experiencia laboral/Liderando un equipo en IT?',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Si', value: 'yes' },
+          { title: 'No', value: 'no' },
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'workExperience',
+      title: 'Experiencia laboral',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Hasta 6 meses (Solo participantes)', value: 'level0' },
+          { title: 'Entre 6 meses y 1 año (Solo participantes)', value: 'level1' },
+          { title: 'Más de un año (Solo participantes)', value: 'level2' },
+          { title: 'Hasta 1 año (Solo líderes)', value: 'level3' },
+          { title: 'Entre 1 año y 3 años (Solo líderes)', value: 'level4' },
+          { title: 'Más de 3 años (Solo líderes)', value: 'level5' },
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'stackWanted',
+      title: '¿Te interesa front (React) o back (Node/Express)?',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Frontend (React)', value: 'front' },
+          { title: 'Backend (Node/Express)', value: 'back' },
+          { title: 'Ambas', value: 'both' },
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
       name: 'timeAvailability',
       title: 'Disponibilidad horaria',
       type: 'string',
@@ -56,27 +117,28 @@ export default {
       validation: (Rule) => Rule.required(),
     },
     {
-      name: 'experience',
-      title: '¿Ya tienes experiencia laboral en IT?',
+      name: 'projects',
+      title: '¿Que proyecto te interesa más?',
       type: 'string',
       options: {
         list: [
-          { title: 'Si', value: 'yes' },
-          { title: 'No', value: 'no' },
+          { title: 'App Flashcards', value: 'flashcards' },
+          { title: 'App Administrador call for papers', value: 'callForPapers' },
+          { title: 'No tengo preferencias', value: 'both' },
         ],
       },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'previousKnowledge',
+      title: 'Conocimientos previos',
+      type: 'text',
       validation: (Rule) => Rule.required(),
     },
     {
       name: 'aboutParticipant',
       title:
         'Contanos algo sobre vos y por qué te interesa formar parte de este proyecto',
-      type: 'text',
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      name: 'previousKnowledge',
-      title: 'Conocimientos previos',
       type: 'text',
       validation: (Rule) => Rule.required(),
     },
